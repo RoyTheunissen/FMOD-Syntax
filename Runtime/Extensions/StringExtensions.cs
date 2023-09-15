90f3e36df35151a7116aea57607ca7af06bf7b76
Roy Theunissen <roy.theunissen@live.nl>
2023-09-15 12:11:41 +0200

    Removed unused string extensions.

--- Runtime/Extensions/StringExtensions.cs
+++ Runtime/Extensions/StringExtensions.cs
@@ -10,31 +10,6 @@ namespace RoyTheunissen.FMODWrapper
         private const char Underscore = '_';
         public const char DefaultSeparator = ' ';
 
-        // Courtesy of 'ruffin' from Stack Overflow: https://pastebin.com/w6aPDn3x
-        public static int IndexOfNth(this string text, string value, int occurrenceIndex)
-        {
-            int result = -1;
-
-            int count = 0;
-            int n = 0;
-
-            while ((n = text.IndexOf(value, n)) != -1 && count < occurrenceIndex)
-            {
-                n++;
-                count++;
-            }
-
-            if (count == occurrenceIndex)
-                result = n;
-
-            return result;
-        }
-
-        public static int Count(this string text, string value)
-        {
-            return (text.Length - text.Replace(value, "").Length) / value.Length;
-        }
-
         private static bool IsExcludedSymbol(char symbol, char wordSeparator = DefaultSeparator)
         {
             return char.IsWhiteSpace(symbol) || char.IsPunctuation(symbol) || symbol == wordSeparator;
@@ -85,15 +60,6 @@ namespace RoyTheunissen.FMODWrapper
                 return camelCasingText.ToUpper();
 
             return char.ToUpper(camelCasingText[0]) + camelCasingText.Substring(1);
-        }
-
-        public static string ToScreamCasing(this string text, char wordSeparator = Underscore)
-        {
-            if (string.IsNullOrEmpty(text))
-                return text;
-
-            // Split it up into human readable words first, then simplify the casing and separation.
-            return text.ToHumanReadable(wordSeparator).ToUpper();
         }
 
         /// <summary>
@@ -179,61 +145,6 @@ namespace RoyTheunissen.FMODWrapper
             return result;
         }
 
-        /// <summary>
-        /// Splits up a string like Footstep_02 into Footstep_ (root) and 02 (numberSuffix).
-        /// </summary>
-        public static void GetNumberSuffix(this string name, out string root, out string numberSuffix)
-        {
-            GetNumberSuffix(name, false, out root, out numberSuffix);
-        }
-
-        /// <summary>
-        /// Splits up a string like Footstep_02 into Footstep_ (root) and 02 (numberSuffix).
-        /// </summary>
-        public static void GetNumberSuffix(
-            this string name, bool includeSeparatorsInNumber, out string root, out string numberSuffix)
-        {
-            numberSuffix = string.Empty;
-
-            if (string.IsNullOrEmpty(name))
-            {
-                root = string.Empty;
-                return;
-            }
-
-            for (int i = name.Length - 1; i >= 0; i--)
-            {
-                if (char.IsNumber(name[i]) || includeSeparatorsInNumber && (name[i] == ' ' || name[i] == '_'))
-                {
-                    numberSuffix = name[i] + numberSuffix;
-                    continue;
-                }
-
-                break;
-            }
-
-            root = name.Substring(0, name.Length - numberSuffix.Length);
-        }
-
-        /// <summary>
-        /// Splits up a string with parentheses like Footstep (New) into Footstep (root) and (New) (parentheses).
-        /// </summary>
-        public static void GetParenthesesSuffix(this string name, out string root, out string parenthesesSuffix)
-        {
-            root = name;
-            parenthesesSuffix = string.Empty;
-
-            if (string.IsNullOrEmpty(name))
-                return;
-
-            if (!name.EndsWith(")"))
-                return;
-
-            int parenthesesStart = name.LastIndexOf("(");
-            root = name.Substring(0, parenthesesStart);
-            parenthesesSuffix = name.Substring(parenthesesStart);
-        }
-
         public static string RemovePrefix(this string name, string prefix)
         {
             if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(prefix))
@@ -243,22 +154,6 @@ namespace RoyTheunissen.FMODWrapper
                 return name;
 
             return name.Substring(prefix.Length);
-        }
-
-        public static string RemovePrefix(this string name, char prefix)
-        {
-            return name.RemovePrefix(prefix.ToString());
-        }
-
-        public static string ChangePrefix(this string name, string from, string to)
-        {
-            if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(from))
-                return name;
-
-            if (!name.StartsWith(from))
-                return name;
-
-            return to + name.Substring(from.Length);
         }
 
         public static string RemoveSuffix(this string name, string suffix)
@@ -272,95 +167,6 @@ namespace RoyTheunissen.FMODWrapper
             return name.Substring(0, name.Length - suffix.Length);
         }
 
-        public static string RemoveSuffix(this string name, char suffix)
-        {
-            return name.RemoveSuffix(suffix.ToString());
-        }
-
-        public static string ChangeSuffix(this string name, string from, string to)
-        {
-            if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(from))
-                return name;
-
-            if (!name.EndsWith(from))
-                return name;
-
-            return name.Substring(0, name.Length - from.Length) + to;
-        }
-
-        public static bool TryGetNumberSuffix(this string name, out int number)
-        {
-            number = -1;
-
-            if (!name.EndsWith(')'))
-                return false;
-
-            int openingParenthesis = name.LastIndexOf('(');
-            if (openingParenthesis == -1)
-                return false;
-
-            string suffix = name.Substring(openingParenthesis + 1);
-            suffix = suffix.Substring(0, suffix.Length - 1);
-
-            bool hasValidNumber = int.TryParse(suffix, out int parsedNumber);
-            if (!hasValidNumber)
-                return false;
-
-            number = parsedNumber;
-            return true;
-        }
-
-        public static string SetNumberSuffix(this string name, int number)
-        {
-            if (!name.EndsWith(')'))
-                return $"{name} ({number})";
-
-            int openingParenthesis = name.LastIndexOf('(');
-            if (openingParenthesis == -1)
-            {
-                // Remove the closing parenthesis and any trailing spaces. 
-                name = name.Substring(0, name.Length - 1);
-                name = name.TrimEnd(' ');
-                return $"{name} ({number})";
-            }
-
-            return $"{name.Substring(0, openingParenthesis)}({number.ToString()})";
-        }
-
-        public static void GetUnderscoreSuffix(this string name, out string root, out string underscoreSuffix)
-        {
-            root = name;
-            underscoreSuffix = string.Empty;
-
-            if (string.IsNullOrEmpty(name))
-                return;
-
-            int suffixStart = name.LastIndexOf("_");
-
-            if (suffixStart == -1)
-                return;
-
-            root = name.Substring(0, suffixStart);
-            underscoreSuffix = name.Substring(suffixStart);
-        }
-
-        public static void GetSpaceSuffix(this string name, out string root, out string spaceSuffix)
-        {
-            root = name;
-            spaceSuffix = string.Empty;
-
-            if (string.IsNullOrEmpty(name))
-                return;
-
-            int suffixStart = name.LastIndexOf(" ");
-
-            if (suffixStart == -1)
-                return;
-
-            root = name.Substring(0, suffixStart);
-            spaceSuffix = name.Substring(suffixStart);
-        }
-
         /// <summary>
         /// Converts the slashes to be consistent.
         /// </summary>
@@ -368,9 +174,6 @@ namespace RoyTheunissen.FMODWrapper
         {
             return name.Replace(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar);
         }
-
-        private static readonly char[] DirectorySeparators =
-            {Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar};
 
         private const string AssetsFolder = "Assets";
 
@@ -383,39 +186,6 @@ namespace RoyTheunissen.FMODWrapper
         {
             string absolutePath = projectPath.ToUnityPath().RemoveAssetsPrefix();
             return Application.dataPath + Path.AltDirectorySeparatorChar + absolutePath;
-        }
-
-        public static string GetProjectPath(this string absolutePath)
-        {
-            string projectPath = Application.dataPath.RemoveSuffix(AssetsFolder)
-                .RemoveSuffix(Path.AltDirectorySeparatorChar);
-            string relativePath = Path.GetRelativePath(projectPath, absolutePath).ToUnityPath();
-            return relativePath;
-        }
-
-        public static bool HasParentDirectory(this string path)
-        {
-            return path.LastIndexOfAny(DirectorySeparators) != -1;
-        }
-
-        public static string GetParentDirectory(this string path)
-        {
-            int lastDirectorySeparator = path.LastIndexOfAny(DirectorySeparators);
-            if (lastDirectorySeparator == -1)
-                return path;
-
-            return path.Substring(0, lastDirectorySeparator);
-        }
-
-        public static bool StartsWithAny(this string path, params string[] prefixes)
-        {
-            foreach (string prefix in prefixes)
-            {
-                if (path.StartsWith(prefix))
-                    return true;
-            }
-
-            return false;
         }
 
         public static string GetWhitespacePreceding(this string text, int index, bool includingNewLines)
@@ -436,26 +206,6 @@ namespace RoyTheunissen.FMODWrapper
             }
 
             return whitespacePreceding;
-        }
-
-        public static string GetWhitespaceSucceeding(this string text, int index, bool includingNewLines)
-        {
-            string whitespaceSucceeding = string.Empty;
-            if (index > 0)
-            {
-                for (int i = index; i < text.Length; i++)
-                {
-                    char c = text[i];
-                    if ((c == '\n' || c == '\r') && !includingNewLines)
-                        break;
-                    if (char.IsWhiteSpace(c))
-                        whitespaceSucceeding = text[i] + whitespaceSucceeding;
-                    else
-                        break;
-                }
-            }
-
-            return whitespaceSucceeding;
         }
 
         public static string GetSection(this string text, string from, string to)
