f3fab2938880154d35f979d282bb49db2ffc8eb1
Roy Theunissen <roy.theunissen@live.nl>
2025-09-12 13:40:27 +0200

    Added extension methods for float and int

---
+++ Scripts/Runtime/Extensions/FloatExtensions.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+namespace RoyTheunissen.FMODSyntax
+{
+    public static class FloatExtensions
+    {
+        // These methods break my naming convention of methods always having to be verbs but their
+        // use is unambiguous and the whole point of these methods is to make floating point based 
+        // comparisons more compact and readable.
+        public static bool Approximately(this float a, float b)
+        {
+            return Mathf.Approximately(a, b);
+        }
+    
+        public static bool Approximately(this float a, float b, float tolerance)
+        {
+            return Mathf.Abs(a - b).EqualOrSmaller(tolerance);
+        }
+
+        public static bool Equal(this float a, float b)
+        {
+            return a.Approximately(b);
+        }
+
+        public static bool EqualOrGreater(this float a, float b)
+        {
+            return Equal(a, b) || a > b;
+        }
+
+        public static bool EqualOrSmaller(this float a, float b)
+        {
+            return Equal(a, b) || a < b;
+        }
+
+        public static bool Greater(this float a, float b)
+        {
+            return !Equal(a, b) && a > b;
+        }
+
+        public static bool Smaller(this float a, float b)
+        {
+            return !Equal(a, b) && a < b;
+        }
+        
+        public static float Clamp(this float value, float min, float max)
+        {
+            return Mathf.Clamp(value, min, max);
+        }
+        
+        public static float Saturate(this float value)
+        {
+            return Clamp(value, 0, 1);
+        }
+    }
+}
---
+++ Scripts/Runtime/Extensions/IntExtensions.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+namespace RoyTheunissen.FMODSyntax
+{
+    public static class IntExtensions
+    {
+        public static int Modulo(this int x, int divisor)
+        {
+            return (x % divisor + divisor) % divisor;
+        }
+    
+        public static bool IsEven(this int x)
+        {
+            return x.Modulo(2) == 0;
+        }
+    
+        public static int Abs(this int value)
+        {
+            return Mathf.Abs(value);
+        }
+    
+        public static int Clamp(this int x, int min, int max)
+        {
+            return Mathf.Clamp(x, min, max);
+        }
+    
+        public static int GetSign(this int value)
+        {
+            if (value == 0)
+                return 0;
+            return value > 0 ? 1 : -1;
+        }
+
+        public static int SetSign(this int value, int sign, bool ignoreZero = false)
+        {
+            if (ignoreZero && sign == 0)
+                return value;
+        
+            return value.Abs() * sign.GetSign();
+        }
+    
+        public static int Min(this int value, params int[] values)
+        {
+            return Mathf.Min(value, Mathf.Min(values));
+        }
+    
+        public static int Max(this int value, params int[] values)
+        {
+            return Mathf.Max(value, Mathf.Max(values));
+        }
+    }
+}
