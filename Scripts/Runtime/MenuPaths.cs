bf6e8baaa85fbbd404a332448578154f0b9a93e1
Roy Theunissen <roy.theunissen@live.nl>
2025-09-15 13:01:51 +0200

    Created a central place to define menu paths

---
+++ Scripts/Runtime/MenuPaths.cs
@@ -0,0 +1,12 @@
+namespace RoyTheunissen.FMODSyntax
+{
+    /// <summary>
+    /// Defines useful constants for menu paths so that we don't redefine those over and over.
+    /// </summary>
+    internal static class MenuPaths
+    {
+        public const string ProjectName = "Audio Syntax";
+        public const string CreateScriptableObject = "ScriptableObject/" + ProjectName + "/";
+        public const string CreateSocItem = "ScriptableObject Collection/Collections/" + ProjectName + "/";
+    }
+}
