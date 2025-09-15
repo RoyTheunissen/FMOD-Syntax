a58aea2f26b1692d052a4e962c229eac8ea8cb6a
Roy Theunissen <roy.theunissen@live.nl>
2025-09-15 12:02:39 +0200

    Audio clip events now get a nice slider for the time, when possible.

--- Scripts/Editor/AudioClipEventPropertyDrawer.cs
+++ Scripts/Editor/AudioClipEventPropertyDrawer.cs
@@ -15,11 +15,39 @@ namespace RoyTheunissen.FMODSyntax
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             Rect labelRect = position.GetLabelRect(out Rect valueRect);
+            valueRect.xMin += EditorGUIUtility.standardVerticalSpacing;
+            
             SerializedProperty idProperty = property.FindPropertyRelative("id");
             SerializedProperty timeProperty = property.FindPropertyRelative("time");
 
             EditorGUI.PropertyField(labelRect, idProperty, GUIContent.none);
-            EditorGUI.PropertyField(valueRect, timeProperty, GUIContent.none);
+
+            // First figure out if this event is relative to a specific audio clip. If so we can draw a nice slider.
+            AudioClip audioClip = null;
+            SerializedProperty parentProperty = property.GetParent();
+            string path = parentProperty.propertyPath;
+            if (parentProperty != null)
+            {
+                parentProperty = parentProperty.GetParent();
+                path = parentProperty.propertyPath;
+                if (parentProperty != null)
+                {
+                    SerializedProperty audioClipProperty = parentProperty.FindPropertyRelative("audioClip");
+                    if (audioClipProperty != null && audioClipProperty.objectReferenceValue != null)
+                        audioClip = (AudioClip)audioClipProperty.objectReferenceValue;
+                }
+            }
+
+            // If we know that this event is for a specific audio clip, then draw a nice slider instead of a float
+            // field because that's easier to work with.
+            if (audioClip != null)
+            {
+                EditorGUI.BeginProperty(valueRect, GUIContent.none, timeProperty);
+                timeProperty.floatValue = EditorGUI.Slider(valueRect, timeProperty.floatValue, 0.0f, audioClip.length);
+                EditorGUI.EndProperty();
+            }
+            else
+                EditorGUI.PropertyField(valueRect, timeProperty, GUIContent.none);
         }
     }
 }
