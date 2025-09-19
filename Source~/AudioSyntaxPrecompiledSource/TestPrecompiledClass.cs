025b721204a9b09eee9bcf760d228d48a15d8226
Roy Theunissen <roy.theunissen@live.nl>
2025-09-19 10:39:44 +0200

    Added some test functionality to the precompiled test class

--- Source~/AudioSyntaxPrecompiledSource/TestPrecompiledClass.cs
+++ Source~/AudioSyntaxPrecompiledSource/TestPrecompiledClass.cs
@@ -1,9 +1,12 @@
-﻿namespace AudioSyntaxPrecompiledSource
+﻿using UnityEngine;
+
+namespace AudioSyntaxPrecompiledSource
 {
     public class TestPrecompiledClass
     {
         public static void OhWellHelloThereWorld()
         {
+            Debug.Log($"If you're seeing this, then things are in working order.");
         }
     }
 }
